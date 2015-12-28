1f2cd4f70a71381bc90bcf4cccfb38f25d2856cd
Armored Dragon <Armored-Dragon@users.noreply.github.com>
2015-12-27 21:14:59 -0800

    Finally commit the removal of an unnecessary import in the BIL

--- mineserver.py
+++ mineserver.py
@@ -3,7 +3,6 @@
 
 # This is the new base initialization layer for Mineserver.
 # Always call this instead of server_core/server.py
-from __future__ import print_function
 from server_core.server import *
 import sys
 
