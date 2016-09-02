e984776de612da84055db0b3cb7b44ce81a8dc16
Daniil Yarancev <tiberiumk12@gmail.com>
2016-09-02 07:32:53 +0300

    *IN PROGRESS* Successfull attempt to make another protocol on top of server protocol :)

--- mineserver.py
+++ mineserver.py
@@ -3,7 +3,8 @@
 
 # This is the new base initialization layer for Mineserver.
 # Always call this instead of server_core/server.py
-from server_core.server import *
 import sys
+
+from server_core.core import *
 
 main(sys.argv[1:])
--- server_core/server.py
+++ server_core/server.py
@@ -14,7 +14,6 @@ def u(x):
 
 
 from random import randint
-from sys import exit as sysex
 
 from quarry.net.server import ServerFactory, ServerProtocol
 
@@ -24,11 +23,11 @@ import randomdata
 
 class Mineserver(ServerProtocol):
     def packet_login_start(self, buff):
-        if not options.down:
-            ServerProtocol.packet_login_start(self, buff)
-        else:
-            buff.discard()
-            self.close(options.downmsg)
+        # if not options.down:
+        ServerProtocol.packet_login_start(self, buff)
+        # else:
+        #   buff.discard()
+        #   self.close(options.downmsg)
 
     def player_joined(self):
         ServerProtocol.player_joined(self)
@@ -39,10 +38,8 @@ class Mineserver(ServerProtocol):
         # self.anim_i = 0
         players[self.entity_id] = self
         self.logger.info("UUID of player {0} is {1}".format(self.username, self.uuid))
-        self.game(self.entity_id, 1, 0, 1, options.maxplayers, "default", False)
-
+        self.game(self.entity_id, 1, 0, 1, 50, "default", False)
         self.spawn_pos(0, 66, 0)
-
         self.abilities(True, True, True, True, 0.2, 0.2)
         self.pos_look(0, 66, 0, 0, 0, False)
         self.rain(True)
@@ -59,12 +56,20 @@ class Mineserver(ServerProtocol):
         # pushChat(self, "\u00A7e" + self.username + " has joined the game\u00A7r", 1)
 
         # Send welcome title and subtitle
-        self.title(options.wtitle)
-        self.subtitle(options.wst)
-
+        # self.title(options.wtitle)
+        # self.subtitle(options.wst)
         self.chat_json(randomdata.join_json(self), 1)
-
+        try:
+            self.player_join_event()
+        except Exception as ex:
+            print(ex.message)
+            print("ERROR IN JOIN EVENT!")
     def player_left(self):
+        try:
+            self.player_leave_event()
+        except Exception as ex:
+            print(ex.message)
+            print("ERROR IN LEAVE EVENT!")
         ServerProtocol.player_left(self)
         del players[self.entity_id]
         # pushChatCall(self, "\u00A7e" + self.username + " has left the game\u00A7r", 1, self.destroy)
@@ -199,52 +204,3 @@ def random_digits(n):
     range_start = 10 ** (n - 1)
     range_end = (10 ** n) - 1
     return randint(range_start, range_end)
-
-
-def main(args):
-    # Parse options
-    import optparse
-    parser = optparse.OptionParser(usage="usage: %prog [options]")
-    parser.add_option("-a", "--host", dest="host", default="", help="address to listen on")
-    parser.add_option("-p", "--port", dest="port", default="25565", type="int", help="port to listen on")
-    parser.add_option("-m", "--motd", dest="motd", default="PyMineserver: Test. Hello! Now with joining! §b§l\\o/§r",
-                      type="string", help="motd to send to clients")
-    parser.add_option("-o", "--offline", action="store_false", dest="auth", default=True,
-                      help="offline mode does not authenticate players!")
-    parser.add_option("-k", "--downtime", action="store_true", dest="down", default=False,
-                      help="kick players with downtimemsg")
-    parser.add_option("-q", "--downtimemsg", dest="downmsg",
-                      default="Sorry, but this server is currently down for maintenance. Check back soon!",
-                      help="message to kick for downtime with")
-    parser.add_option("-w", "--wtitle", dest="wtitle", default="Welcome to Mineserver!",
-                      help="title to display on join")
-    parser.add_option("-s", "--wsubtitle", dest="wst", default="Enjoy this test server!",
-                      help="subtitle to display on join")
-    parser.add_option("-l", "--max-players", dest="maxplayers", default=20, help="max player count/limit")
-    parser.add_option("-f", "--favicon", dest="favicon", default="creeper.png",
-                      help="relative path to server favicon in png")
-    global options
-    (options, args) = parser.parse_args(args)
-
-    # Warn about auth mode
-    if options.auth:
-        print(
-            "Mineserver (warn/INFO)> Mineserver is running in online mode. All players must be authenticated to join. *(ONLINE)*")
-    else:
-        print(
-            "Mineserver (WARN/info)> Mineserver is running in offline mode. Players can join with fake UUIDs and names without authentication! *[OFFLINE]*!")
-
-    # Create factory
-    factory = MineFactory()
-    factory.motd = options.motd
-    factory.online_mode = options.auth
-    factory.favicon = options.favicon
-
-    # Listen
-    factory.listen(options.host, options.port)
-    factory.run()
-
-
-if __name__ == "__main__":
-    print("Mineserver (warn/CRIT)> You should NOT be invoking this directly! Use mineserver.py.")
-    sysex(1)
