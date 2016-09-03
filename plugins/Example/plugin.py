4a99e2043a8d223dde61d14779dddca20347a884
Daniil Yarancev <tiberiumk12@gmail.com>
2016-09-03 17:48:00 +0300

    add activate and deactivate methods (from yapsy)

--- plugins/Example/plugin.py
+++ plugins/Example/plugin.py
@@ -9,8 +9,16 @@ class Plugin(IPlugin):
         super(Plugin, self).__init__()  # Call super's method in order to initialize plugin...
         # Then you can set your own global variables (for all players)...
 
+    # This is being called when plugin initializes..
+    def activate(self):
+        pass
+
+    # This is being called when plugin shuts down..
+    def deactivate(self):
+        pass
+
     # Player argument must be ALWAYS present - it's player object
-    #You can init player variables here (or in any another event)
+    # You can init player variables here (or in any another event)
     def player_join_event(self, player):
         player.x = 0
         player.y = 0
