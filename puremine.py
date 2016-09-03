593d5aafc27d88b6904a64052e48fb68c93f90b0
Daniil Yarancev <tiberiumk12@gmail.com>
2016-09-03 14:23:29 +0300

    removed todo, changed some config reading code

--- puremine.py
+++ puremine.py
@@ -3,7 +3,6 @@
 
 # This is the new base initialization layer for Puremine.
 # Always call this instead of server_core/core.py
-# TODO: Create and read config with all parameters(like bukkit or spigot do)
 from server_core.core import main
 
 
@@ -24,11 +23,11 @@ try:
         for line in config.readlines():
             if not line.startswith("#"):  # If line isn't a comment
                 key, value = line.strip().split('=')  # Split key and value
-                if line[1] != "":  # If value isn't empty
-                    if line[1] in ('True', 'false', 'true', 'False'):  # If line is a boolean
+                if value != "":  # If value isn't empty
+                    if value in ('True', 'false', 'true', 'False'):  # If line is a boolean
                         properties[key] = str2bool(value)  # Add key and value to our dictionary
                         continue
-                    elif line[1].isdigit():  # If line is a number
+                    elif value.isdigit():  # If line is a number
                         properties[key] = int(value)
                         continue
                     else:
