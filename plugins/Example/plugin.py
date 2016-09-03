ec01885ad8c0f69676709e509f4c4a8152405df0
Daniil Yarancev <tiberiumk12@gmail.com>
2016-09-03 17:43:29 +0300

    add global and player-specific variables fixed typos

--- plugins/Example/plugin.py
+++ plugins/Example/plugin.py
@@ -4,18 +4,17 @@ from yapsy.IPlugin import IPlugin
 
 # And you must ALWAYS add IPlugin as base class to your's
 class Plugin(IPlugin):
-    #If you don't need to set your own long-living variables - you don't need __init__
+    # If you don't need to set your global variables(across all players) - you don't need __init__
     def __init__(self):
         super(Plugin, self).__init__()  # Call super's method in order to initialize plugin...
-        # Then you can set your own stuff...
-        self.hello_message = "Hello!!!"
-        self.x = 0
-        self.y = 0
-        self.z = 0
+        # Then you can set your own global variables (for all players)...
 
     # Player argument must be ALWAYS present - it's player object
+    #You can init player variables here (or in any another event)
     def player_join_event(self, player):
-        print(self.hello_message)
+        player.x = 0
+        player.y = 0
+        player.z = 0
         print(player.username)
 
     def player_leave_event(self, player):
@@ -23,12 +22,14 @@ class Plugin(IPlugin):
         print(player.username)
 
     # You need to add arguments to some event, this arguments will be in docs
+    #So we check there if player movevent on X is bigger than 5 units
     def player_move_event(self, player, x, y, z, on_ground):
-        if (self.x - x) > 5:
+        if (player.x - x) > 3:
             print("X delta is bigger than FIVE!")
-        self.x = x
-        self.y = y
-        self.z = z
+        # This variables are not equal for different players :)
+        player.x = x
+        player.y = y
+        player.z = z
 
     def player_chat_event(self, player, message):
         print("player chat from plug-in " + str(message))
